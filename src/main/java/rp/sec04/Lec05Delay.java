1cb8ec6be6e802cfad66053479451e39f8045f95
INTENCER\k.michalski <krzysztof.michalski@intive.com>
2023-05-31 22:39:29 +0200

    improve sec 04

--- src/main/java/rp/sec04/Lec05Delay.java
+++ src/main/java/rp/sec04/Lec05Delay.java
@@ -3,14 +3,21 @@ package rp.sec04;
 import reactor.core.publisher.Flux;
 import reactor.core.scheduler.Scheduler;
 import reactor.core.scheduler.Schedulers;
+import reactor.util.concurrent.Queues;
 import rp.courseutil.Util;
 
 import java.time.Duration;
 
+
 public class Lec05Delay {
     public static void main(String[] args) {
-        Flux.range(1, 1000).log()
-                .delayElements(Duration.ofSeconds(1), Schedulers.parallel())
+        System.setProperty("reactor.bufferSize.x", "9");
+        Flux.range(1, 100)
+                .log()
+                .limitRate(10, 2)
+                .delayElements(Duration.ofSeconds(1))
                 .subscribe(Util.subscriber());
+
+        Util.sleepSeconds(60);
     }
 }
